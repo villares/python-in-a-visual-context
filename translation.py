a7b91efd79f6072e7d3af376457384f78515bcdd
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-04-15 09:35:22 -0300

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,11 +1,9 @@
 # @name: translate.py
-
 # @license: GNU Affero General Public License, Version 3 <https://www.gnu.org/licenses/agpl-3.0.en.html>
 # @author: Alexandre Villares
 # @purpose: Translate Markdown files
 # @acknowledgements:
-# Based on Simon Bowie <ad7588@coventry.ac.uk> work
-
+# Based on work by Simon Bowie <ad7588@coventry.ac.uk> 
 
 from time import sleep
 import json
@@ -27,27 +25,13 @@ output_directory = Path('/home/villares/GitHub/python-visual-context/Processing-
  
 # iterate over files with .md suffix in the input directory
 input_files = [fp.relative_to(input_directory) for fp in Path(input_directory).rglob("*.md")] 
+print(input_files)
 output_files = [fp.relative_to(output_directory) for fp in Path(output_directory).rglob("*.md")] 
 
 to_do = set(input_files) - set(output_files)
-
 for file_path in to_do:
     # read Markdown file
     text = frontmatter.load(input_directory / file_path)
-#     # send content of Markdown file to LibreTranslate API
-#     payload = {
-#         "q": text.content,
-#         "source": input_language,
-#         "target": output_language,
-#         "format": "text",
-#         "api_key": ""
-#     }
-#     headers = {"Content-Type": "application/json"}
-#     
-    #response = requests.post(url, data=json.dumps(payload), headers=headers)
-    #print(response)
-    #data = response.json()
-    #text.content = data['translatedText']
     try:
         text.content = lt.translate(text, input_language, output_language)
     except urllib.error.HTTPError:
@@ -62,5 +46,3 @@ for file_path in to_do:
     # write new Markdown file
     with open(output_directory / file_path, 'w') as f:
         f.write(frontmatter.dumps(text))
-
-
